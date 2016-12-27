bc64195dff26fc447c6b61249a8bcc2b2c381a00
Carlos Cabot <carloscabotdp@gmail.com>
2016-12-27 19:08:40 +0100

    Functionality Create new exam - Frontend

---
+++ crear_examen.js
@@ -0,0 +1,100 @@
+//Variables globales
+var rootURL = "src/public";
+var array_temas;
+// Objeto JSON
+var examen = {
+    nombre: ""
+    , id_tema: ""
+    , examen_json: ""
+    , porcentaje_aprovar: undefined
+    , tiempo_minutos: undefined
+    , descripcion: ""
+}
+
+var array_preguntas = [];
+
+// Equivalente a document.ready()
+$(function () {
+    
+    //Obtener todos los temas de examen de la api
+    $.ajax({
+        type: 'GET'
+        , url: rootURL + '/temas'
+        , dataType: "json"
+        , success: function (response) {
+            array_temas = response;
+        }
+        , error: function (jqXHR, textStatus, errorThrown) {
+            alert(textStatus);
+        }
+    });
+    
+    // Funcion al evento click del boton crear_examen
+    $('body').on('click', '#crear_examen', function () {      
+        // Eliminar el boton crear examen
+        $('#crear_examen').remove();        
+        // Creación de un select con los temas disponibles de la api y el resto de inputs excepto el campo examen_json
+        var formContentTemas = '<select id="temaSelection" name="temaSelection"><option value="">Seleccionar Tema</option>';
+        $.each(array_temas, function (index, value) {
+            formContentTemas += '<option id="' + value.id_tema + '"' + 'value="' + value.id_tema + '">' + value.nombre + '</option>';
+        });
+        formContentTemas += '</select>';        
+        $('#contenedor').append(formContentTemas + '<br><input type="text" id="nombre_exam" name="nombre_exam" value="" placeholder="Nombre del examen"><br><input type="number" id="porcentaje" name="porcentaje" value="" placeholder="Porcentaje de aciertos"><br><input type="number" id="tiempo_minutos" name="tiempo_minutos" value="" placeholder="Duración examen"><br><textarea id="descipcion" rows="4" cols="50" placeholder="Descripción del examen"></textarea><br><button id="aceptar_tema_nombre">Aceptar</button>');
+    });
+       
+    // Almacenar datos examen en la variable
+    $('body').on('click', '#aceptar_tema_nombre', function () {
+            examen.id_tema = $('#temaSelection option:selected').val();
+            examen.nombre = $('#nombre_exam').val();
+            examen.porcentaje_aprovar = $('#porcentaje').val();
+            examen.tiempo_minutos = $('#tiempo_minutos').val();
+            examen.descripcion = $('#descipcion').val();
+        
+            // Eliminar los inputs previos
+            $('#contenedor').empty();     
+        
+            // Añadir los inputs para nueva pregunta
+            $('#contenedor').append('<input type="text" id="enunciado" name="enunciado" value="" placeholder="Enunciado de la pregunta"><br><input type="text" id="respuesta_A" name="respuesta_A" value="" placeholder="Respuesta A"><br><input type="text" id="respuesta_B" name="respuesta_B" value="" placeholder="Respuesta B"><br><input type="text" id="respuesta_C" name="respuesta_C" value="" placeholder="Respuesta C"><br><input type="text" id="respuesta_D" name="respuesta_D" value="" placeholder="Respuesta D"><br><input class="solucion" type="radio" name="solucion" value="A"> A <br><input class="solucion" type="radio" name="solucion" value="B"> B <br><input class="solucion" type="radio" name="solucion" value="C"> C <br><input class="solucion" type="radio" name="solucion" value="D"> D <br><button id="nueva_pregunta">Añadir Pregunta</button><button id="guardar_examen">Guardar examen</button>');       
+    });
+    
+    // Funcionalidad del boton "Añadir pregunta"
+    $('body').on('click', '#nueva_pregunta', function () {            
+        var pregunta = '{"id_pregunta":'+ array_preguntas.length+1 +',"enunciado":"'+ $('#enunciado').val() +'","respuestas":{"A":"'+ $('#respuesta_A').val() +'","B":"'+ $('#respuesta_B').val() +'","C":"'+ $('#respuesta_C').val() +'","D":"'+ $('#respuesta_D').val() +'","solucion":"'+ $('input[name=solucion]:checked').val() +'"}}';    
+         
+        //Añadir la pregunta al array
+        array_preguntas.push(pregunta);
+        
+        //Limpiar los campos input
+        $('#enunciado').val('');
+        $('#respuesta_A').val('');
+        $('#respuesta_B').val('');
+        $('#respuesta_C').val('');
+        $('#respuesta_D').val('');
+        $('.solucion').prop('checked', false);     
+    });
+
+    // Funcionalidad del boton Guardar examen
+    $('body').on('click', '#guardar_examen', function () {
+        examen.examen_json += '[';
+        for(var i=0; i < array_preguntas.length; i++){
+            examen.examen_json += array_preguntas[i];
+            if(i+1 < array_preguntas.length){
+                examen.examen_json += ",";
+            }
+        }
+        examen.examen_json += ']';
+        
+        $.ajax({
+            type: 'post'
+            , url: rootURL + '/examen/new'
+            , dataType: "json"
+            , data: { nombre: examen.nombre, id_tema: examen.id_tema, examen_json: examen.examen_json, porcentaje_aprovar: examen.porcentaje_aprovar, tiempo_minutos: examen.tiempo_minutos, descripcion: examen.descripcion }
+            , success: function (data, textStatus, jqXHR) {
+                alert('Exam created successfully');
+            }
+            , error: function (jqXHR, textStatus, errorThrown) {
+                alert('error: ' + textStatus);
+            }
+        }); 
+    });  
+});
--- src/public/index.php
+++ src/public/index.php
@@ -188,7 +188,7 @@ $app->post('/tema/new', function (Request $request, Response $response){
 $app->post('/examen/new', function (Request $request, Response $response){       
     //Getting parsed data from request 
     //Example data:
-    //{ nombre: 'Angular', id_tema: 1, examen_json: '[{"id_pregunta":1,"enunciado":"¿Como se denomina el lenguaje de estilos para navegador?","respuestas":{"A":"CSS","B":"HTML","C":"Javascript","D":"XML","solucion":"A"}},{"id_pregunta":2,"enunciado":"¿Como definiriamos el software Brackets?","respuestas":{"A":"como un IDE","B":"como un editor de texto","C":"como lenguaje de programación","D":"como aparatos","solucion":"B"}},{"id_pregunta":3,"enunciado":"¿Donde se ejecuta el lenguaje PHP?","respuestas":{"A":"En le navegador","B":"En mi casa","C":"En el servidor de aplicaciones php","D":"En ninguna de las anteriores","solucion":"C"}},{"id_pregunta":4,"enunciado":"¿Las siglas HTML son de?","respuestas":{"A":"Lenguaje de Programación de Hipertexto","B":"Lenguaje de Marcas de Ropa","C":"HyperText Markdown Language","D":"Lenguaje de Marcas de Hipertexto","solucion":"D"}},{"id_pregunta":5,"enunciado":"¿Finalizaremos el proyecto a tiempo?","respuestas":{"A":"Puede","B":"No","C":"Hombre claro","D":"¿Que proyecto?","solucion":"C"}}]', porcentaje_aprovar: 50, tiempo_minutos: 15}
+    //{ nombre: 'Angular', id_tema: 1, examen_json: '[{"id_pregunta":1,"enunciado":"¿Como se denomina el lenguaje de estilos para navegador?","respuestas":{"A":"CSS","B":"HTML","C":"Javascript","D":"XML","solucion":"A"}},{"id_pregunta":2,"enunciado":"¿Como definiriamos el software Brackets?","respuestas":{"A":"como un IDE","B":"como un editor de texto","C":"como lenguaje de programación","D":"como aparatos","solucion":"B"}},{"id_pregunta":3,"enunciado":"¿Donde se ejecuta el lenguaje PHP?","respuestas":{"A":"En le navegador","B":"En mi casa","C":"En el servidor de aplicaciones php","D":"En ninguna de las anteriores","solucion":"C"}},{"id_pregunta":4,"enunciado":"¿Las siglas HTML son de?","respuestas":{"A":"Lenguaje de Programación de Hipertexto","B":"Lenguaje de Marcas de Ropa","C":"HyperText Markdown Language","D":"Lenguaje de Marcas de Hipertexto","solucion":"D"}},{"id_pregunta":5,"enunciado":"¿Finalizaremos el proyecto a tiempo?","respuestas":{"A":"Puede","B":"No","C":"Hombre claro","D":"¿Que proyecto?","solucion":"C"}}]', porcentaje_aprovar: 50, tiempo_minutos: 15, descripcion: 'esto es la descripcion'}
     $object = $request->getParsedBody();    
     
     // Extract the variables needed to insert a new evaluation into the Database
@@ -197,8 +197,9 @@ $app->post('/examen/new', function (Request $request, Response $response){
     $examen_json = $object['examen_json'];     
     $porcentaje_aprovar = $object['porcentaje_aprovar'];
     $tiempo_minutos = $object['tiempo_minutos']; 
+    $descripcion = $object['descripcion']; 
         
-    $sth = $this->db->prepare(" INSERT INTO examen VALUES (NULL, '$nombre',  $id_tema, '$examen_json', $porcentaje_aprovar, $tiempo_minutos)");
+    $sth = $this->db->prepare(" INSERT INTO examen VALUES (NULL, '$nombre',  $id_tema, '$examen_json', $porcentaje_aprovar, $tiempo_minutos, '$descripcion' )");
     $sth->execute();     
 });
 
