01fe8c5e19616071db2f2c95353a32cf6656f78d
Carlos Cabot <carloscabotdp@gmail.com>
2017-01-05 15:18:31 +0100

    Bootstrap style to crear_examen

--- js/crear_examen.js
+++ js/crear_examen.js
@@ -33,13 +33,53 @@ $(function () {
     $('body').on('click', '#crear_examen', function () {      
         // Eliminar el boton crear examen
         $('#crear_examen').remove();        
+        
         // Creación de un select con los temas disponibles de la api y el resto de inputs excepto el campo examen_json
-        var formContentTemas = '<select id="temaSelection" name="temaSelection"><option value="">Seleccionar Tema</option>';
+        var formContentTemas = 
+        '<form action="#" class="form-horizontal" onsubmit="return false">'
+        + '<div class="form-group">'
+            + '<label for="temaSelection" class="col-sm-3 control-label">Seleccionar Tema:</label>'
+            + '<div class="col-sm-9">'
+                + '<select id="temaSelection" name="temaSelection" class="form-control">'
+                    + '<option>Seleccionar Tema</option>';
+        
         $.each(array_temas, function (index, value) {
             formContentTemas += '<option id="' + value.id_tema + '"' + 'value="' + value.id_tema + '">' + value.nombre + '</option>';
         });
-        formContentTemas += '</select>';        
-        $('#contenedor').append(formContentTemas + '<br><input type="text" id="nombre_exam" name="nombre_exam" value="" placeholder="Nombre del examen"><br><input type="number" id="porcentaje" name="porcentaje" value="" placeholder="Porcentaje de aciertos"><br><input type="number" id="tiempo_minutos" name="tiempo_minutos" value="" placeholder="Duración examen"><br><textarea id="descipcion" rows="4" cols="50" placeholder="Descripción del examen"></textarea><br><button id="aceptar_tema_nombre">Aceptar</button>');
+        
+        formContentTemas += '</select></div></div>';        
+        $('#contenedor').append(formContentTemas 
+            + '<div class="form-group">'
+                + '<label for="temaSelection" class="col-sm-3 control-label">Nombre examen: </label>' 
+                + '<div class="col-sm-9">' 
+                    + '<input type="text" class="form-control" id="nombre_exam" name="nombre_exam" placeholder="Nombre del examen">' 
+                + '</div>' 
+            + '</div>'
+            + '<div class="form-group">' 
+                + '<label for="temaSelection" class="col-sm-3 control-label">Porcentaje de aciertos: </label>' 
+                + '<div class="col-sm-9">'
+                    + '<input type="number" id="porcentaje" name="porcentaje" class="form-control" placeholder="Porcentaje de aciertos">' 
+                + '</div>' 
+            + '</div>' 
+            + '<div class="form-group">' 
+                + '<label for="temaSelection" class="col-sm-3 control-label">Duración examen: </label>'
+                + '<div class="col-sm-9">' 
+                    + '<input type="number" id="tiempo_minutos" name="tiempo_minutos" class="form-control" placeholder="Duración examen">' 
+                + '</div>' 
+            + '</div>'
+            + '<div class="form-group">'
+                + '<label for="temaSelection" class="col-sm-3 control-label">Descripción del examen: </label>'
+                + '<div class="col-sm-9">'
+                    + '<textarea id="descipcion" rows="4" cols="50" placeholder="Descripción del examen" class="form-control"></textarea>' 
+                + '</div>'
+            + '</div>'
+            + '<div class="form-group">'
+                + '<label for="temaSelection" class="col-sm-3 control-label"></label>' 
+                + '<div class="col-sm-9">' 
+                    + '<button id="aceptar_tema_nombre" class="btn btn-primary">Crear preguntas</button>' 
+                + '</div>'
+            + '</div>' 
+        + '</form>');
     });
        
     // Almacenar datos examen en la variable
@@ -54,7 +94,58 @@ $(function () {
             $('#contenedor').empty();     
         
             // Añadir los inputs para nueva pregunta
-            $('#contenedor').append('<input type="text" id="enunciado" name="enunciado" value="" placeholder="Enunciado de la pregunta"><br><input type="text" id="respuesta_A" name="respuesta_A" value="" placeholder="Respuesta A"><br><input type="text" id="respuesta_B" name="respuesta_B" value="" placeholder="Respuesta B"><br><input type="text" id="respuesta_C" name="respuesta_C" value="" placeholder="Respuesta C"><br><input type="text" id="respuesta_D" name="respuesta_D" value="" placeholder="Respuesta D"><br><input class="solucion" type="radio" name="solucion" value="A"> A <br><input class="solucion" type="radio" name="solucion" value="B"> B <br><input class="solucion" type="radio" name="solucion" value="C"> C <br><input class="solucion" type="radio" name="solucion" value="D"> D <br><button id="nueva_pregunta">Añadir Pregunta</button><button id="guardar_examen">Guardar examen</button>');       
+            $('#contenedor').append(
+            '<form action="#" class="form-horizontal" onsubmit="return false">'
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label">Enunciado: </label>' 
+                    + '<div class="col-sm-9">' 
+                        + '<input type="text" id="enunciado" name="enunciado" class="form-control" placeholder="Enunciado de la pregunta">'
+                    + '</div>'
+                + '</div>'           
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label">Respuesta A: </label>' 
+                    + '<div class="col-sm-9">'                     
+                        + '<input type="text" id="respuesta_A" name="respuesta_A" class="form-control" placeholder="Respuesta A">' 
+                    + '</div>'
+                + '</div>'
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label">Respuesta B: </label>' 
+                    + '<div class="col-sm-9">'                     
+                        + '<input type="text" id="respuesta_B" name="respuesta_B" class="form-control" placeholder="Respuesta B">'
+                    + '</div>'
+                + '</div>'
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label">Respuesta C: </label>' 
+                    + '<div class="col-sm-9">'                     
+                        + '<input type="text" id="respuesta_C" name="respuesta_C" class="form-control" placeholder="Respuesta C">'
+                    + '</div>'
+                + '</div>'
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label">Respuesta D: </label>' 
+                    + '<div class="col-sm-9">' 
+                        + '<input type="text" id="respuesta_D" name="respuesta_D" class="form-control" placeholder="Respuesta D">'
+                    + '</div>'
+                + '</div>'
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label">Solución: </label>' 
+                    + '<div class="col-sm-9">'                                     
+                        + '<label class="checkbox-inline"><input class="solucion" type="radio" name="solucion" value="A"> A </label>'
+                
+                        + '<label class="checkbox-inline"><input class="solucion" type="radio" name="solucion" value="B"> B </label>'
+                
+                        + '<label class="checkbox-inline"><input class="solucion" type="radio" name="solucion" value="C"> C </label>'
+                
+                        + '<label class="checkbox-inline"><input class="solucion" type="radio" name="solucion" value="D"> D </label>'
+                    + '</div>'
+                + '</div>' 
+                + '<div class="form-group">'
+                    + '<label for="temaSelection" class="col-sm-3 control-label"></label>' 
+                    + '<div class="col-sm-9">' 
+                        + '<button id="nueva_pregunta" class="btn btn-default" style="margin-right:5px;">Añadir Pregunta</button>'
+                        + '<button id="guardar_examen" class="btn btn-primary">Guardar examen</button>' 
+                    + '</div>'
+                + '</div>'
+            + '</form>');       
     });
     
     // Funcionalidad del boton "Añadir pregunta"
